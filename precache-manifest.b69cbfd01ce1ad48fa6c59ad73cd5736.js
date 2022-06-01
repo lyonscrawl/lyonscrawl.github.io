181402b9fd82ebe9d58500b8de2c16f28a774651
Yanne Sidibe <yannsid2@gmail.com>
2022-06-01 04:48:35 +0200

    Updates

---
+++ precache-manifest.b69cbfd01ce1ad48fa6c59ad73cd5736.js
@@ -0,0 +1,174 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6ed826c1e9dabae6c44442c34e4bca8a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f5fd0bae0896879f60aa",
+    "url": "/static/css/2.41a0e41b.chunk.css"
+  },
+  {
+    "revision": "3b319e3fc8d838d3441b",
+    "url": "/static/css/main.d6017185.chunk.css"
+  },
+  {
+    "revision": "f5fd0bae0896879f60aa",
+    "url": "/static/js/2.b63bc129.chunk.js"
+  },
+  {
+    "revision": "32eaebd0b06f4bb60d5e9e5d0e7cd13e",
+    "url": "/static/js/2.b63bc129.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3b319e3fc8d838d3441b",
+    "url": "/static/js/main.6bf34506.chunk.js"
+  },
+  {
+    "revision": "ca12f1bef28d1c9d87b2",
+    "url": "/static/js/runtime-main.9f54618c.js"
+  },
+  {
+    "revision": "3d47e0254a79445922a45b5b808d086d",
+    "url": "/static/media/1.3d47e025.JPG"
+  },
+  {
+    "revision": "683c7d62f43188abca53734201653860",
+    "url": "/static/media/2.683c7d62.JPG"
+  },
+  {
+    "revision": "b7c7c1ba5f192ea77920ddc7a4092292",
+    "url": "/static/media/4.b7c7c1ba.jpg"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "4457817ac2b9993c65e81aa05828fe9c",
+    "url": "/static/media/GoogleSans-Bold.4457817a.ttf"
+  },
+  {
+    "revision": "90773b6158663ab0fe78b32680733677",
+    "url": "/static/media/GoogleSans-BoldItalic.90773b61.ttf"
+  },
+  {
+    "revision": "0ecddcdeccb7761ce899aa9ad9f0ac3f",
+    "url": "/static/media/GoogleSans-Italic.0ecddcde.ttf"
+  },
+  {
+    "revision": "8d57e4014b18edef070d285746485115",
+    "url": "/static/media/GoogleSans-Medium.8d57e401.ttf"
+  },
+  {
+    "revision": "8fd3737925e83c87d78a13700ccf9a62",
+    "url": "/static/media/GoogleSans-MediumItalic.8fd37379.ttf"
+  },
+  {
+    "revision": "b5c77a6aed75cdad9489effd0d5ea411",
+    "url": "/static/media/GoogleSans-Regular.b5c77a6a.ttf"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "8eddf7adea62d66b9e4d031b58dfaaa6",
+    "url": "/static/media/SFMono-RegularItalic.8eddf7ad.ttf"
+  },
+  {
+    "revision": "cbea24fad1bfc1a97a05f10396783e09",
+    "url": "/static/media/aircraft_conflict_resolving.cbea24fa.png"
+  },
+  {
+    "revision": "73d246a83c7364c9c7c4182230789dab",
+    "url": "/static/media/cnda.73d246a8.bmp"
+  },
+  {
+    "revision": "592e1d3d88249b69872e67cdd4c0b077",
+    "url": "/static/media/enac.592e1d3d.jpg"
+  },
+  {
+    "revision": "db0dff0ce9d4307a434175362ab8deff",
+    "url": "/static/media/envol.db0dff0c.png"
+  },
+  {
+    "revision": "06147b6cd88c7346cecd1edd060cd5de",
+    "url": "/static/media/fa-brands-400.06147b6c.ttf"
+  },
+  {
+    "revision": "5063b105c7646c8043d58c5289f02cca",
+    "url": "/static/media/fa-brands-400.5063b105.eot"
+  },
+  {
+    "revision": "a9c4bb7348f42626454c988dbde1d0a0",
+    "url": "/static/media/fa-brands-400.a9c4bb73.svg"
+  },
+  {
+    "revision": "c5e0f14f88a828261ba01558ce2bf26f",
+    "url": "/static/media/fa-brands-400.c5e0f14f.woff"
+  },
+  {
+    "revision": "cccc9d29470e879e40eb70249d9a2705",
+    "url": "/static/media/fa-brands-400.cccc9d29.woff2"
+  },
+  {
+    "revision": "65b286af947c0d982ca01b40e1fcab38",
+    "url": "/static/media/fa-regular-400.65b286af.ttf"
+  },
+  {
+    "revision": "7b9568e6389b1f8ae0902cd39665fc1e",
+    "url": "/static/media/fa-regular-400.7b9568e6.svg"
+  },
+  {
+    "revision": "c1a866ec0e04a5e1915b41fcf261457c",
+    "url": "/static/media/fa-regular-400.c1a866ec.eot"
+  },
+  {
+    "revision": "c4f508e7c4f01a9eeba7f08155cde04e",
+    "url": "/static/media/fa-regular-400.c4f508e7.woff"
+  },
+  {
+    "revision": "f5f2566b93e89391da4db79462b8078b",
+    "url": "/static/media/fa-regular-400.f5f2566b.woff2"
+  },
+  {
+    "revision": "0bff33a5fd7ec390235476b4859747a0",
+    "url": "/static/media/fa-solid-900.0bff33a5.ttf"
+  },
+  {
+    "revision": "333bae208dc363746961b234ff6c2500",
+    "url": "/static/media/fa-solid-900.333bae20.woff"
+  },
+  {
+    "revision": "44d537ab79f921fde5a28b2c1636f397",
+    "url": "/static/media/fa-solid-900.44d537ab.woff2"
+  },
+  {
+    "revision": "8e4a6dcc692b3887f9f542cd6894d6d4",
+    "url": "/static/media/fa-solid-900.8e4a6dcc.eot"
+  },
+  {
+    "revision": "c2801fb415f03c7b170934769d7b5397",
+    "url": "/static/media/fa-solid-900.c2801fb4.svg"
+  },
+  {
+    "revision": "0f5a3a896940ffd4e6283aa2aa64eea9",
+    "url": "/static/media/gdg.0f5a3a89.png"
+  },
+  {
+    "revision": "782ea6fcb517e7f2e3e8089e6cb29fb3",
+    "url": "/static/media/huawei.782ea6fc.png"
+  },
+  {
+    "revision": "1d0189743358226b550f5761a4e7ed35",
+    "url": "/static/media/inphb.1d018974.png"
+  },
+  {
+    "revision": "1f796bf433999251f2ffee1a94bc031c",
+    "url": "/static/media/npg.1f796bf4.png"
+  },
+  {
+    "revision": "8d09306c24fafaa60f705fff8b09a936",
+    "url": "/static/media/tgm.8d09306c.png"
+  }
+]);
